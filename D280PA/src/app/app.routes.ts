72cbca3af4c637e87134d844fabd390c0e0d9927
Jesus <reynosoprof@gmail.com>
2024-11-01 15:48:01 -0700

    added home component and redirect route to it

--- D280PA/src/app/app.routes.ts
+++ D280PA/src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {path: " ", redirectTo: "/map" , pathMatch: "full"}, 
+    {path: " ", redirectTo: "/home" , pathMatch: "full"}, 
 ];
