332ca7d4026a44f2e70802775311c9c37a813090
Jesus <reynosoprof@gmail.com>
2024-11-01 16:11:06 -0700

    created separate component to handle the table

--- D280PA/src/app/app.component.ts
+++ D280PA/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { TableComponent } from './table/table.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet , HomeComponent, TableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
