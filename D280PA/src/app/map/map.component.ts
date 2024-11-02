68045d2af10236f05c2925161122dfcf1a4904c3
Jesus <reynosoprof@gmail.com>
2024-11-02 10:13:59 -0700

    cleaned up code, eliminated delay that required a country to be clicked twice

--- D280PA/src/app/map/map.component.ts
+++ D280PA/src/app/map/map.component.ts
@@ -10,58 +10,51 @@ import { MapdataService } from '../mapdata.service';
 })
 export class MapComponent {
   constructor(private mapdata: MapdataService){}
+  
   mapClick(event: MouseEvent){
     const eventTarget = event.target as HTMLElement;
     const cCode = eventTarget.id;
     
-    const test = (this.mapdata.getData(cCode));
-    const cName = (test.querySelector("name")?.textContent) as string;
-    const cNameUpdater = document.getElementById("cTName");
-  if (cNameUpdater) {
-  cNameUpdater.innerHTML = cName;
-  } else {
-  console.error("Element with ID 'cName' not found.");
-  }
+    this.mapdata.getData(cCode)
+    .subscribe(data => {
+      const cName = data.querySelector("name")?.textContent || '';
+      const cCapital = data.querySelector("capitalCity")?.textContent || '';
+      const cRegion = data.querySelector("region")?.textContent || '';
+      const cICL = data.querySelector("incomeLevel")?.textContent || '';
+      const cLat = data.querySelector("latitude")?.textContent || '';
+      const cLon = data.querySelector("longitude")?.textContent || '';
 
-  const cCapital = (test.querySelector("capitalCity")?.textContent) as string;
-    const cCapitalUpdater = document.getElementById("cTCapital");
-  if (cCapitalUpdater) {
-  cCapitalUpdater.innerHTML = cCapital;
-  } else {
-  console.error("Element with ID 'cCapital' not found.");
-  }
+      const cNameUpdater = document.getElementById("cTName");
+      if (cNameUpdater) {
+        cNameUpdater.innerHTML = cName;
+      }
 
-  const cRegion = (test.querySelector("region")?.textContent) as string;
-    const cRegionUpdater = document.getElementById("cTRegion");
-  if (cRegionUpdater) {
-  cRegionUpdater.innerHTML = cRegion;
-  } else {
-  console.error("Element with ID 'cRegion' not found.");
-  }
+      const cCapitalUpdater = document.getElementById("cTCapital");
+      if (cCapitalUpdater) {
+        cCapitalUpdater.innerHTML = cCapital;
+      }
 
-  const cICL = (test.querySelector("incomeLevel")?.textContent) as string;
-    const cICLUpdater = document.getElementById("cTICL");
-  if (cICLUpdater) {
-  cICLUpdater.innerHTML = cICL;
-  } else {
-  console.error("Element with ID 'cICL' not found.");
-  }
+      const cRegionUpdater = document.getElementById("cTRegion");
+      if (cRegionUpdater) {
+        cRegionUpdater.innerHTML = cRegion;
+      }
 
-  const cLat = (test.querySelector("latitude")?.textContent) as string;
-    const cLatUpdater = document.getElementById("cTLat");
-  if (cLatUpdater) {
-  cLatUpdater.innerHTML = cLat;
-  } else {
-  console.error("Element with ID 'cLat' not found.");
-  }
+      const cICLUpdater = document.getElementById("cTICL");
+      if (cICLUpdater) {
+        cICLUpdater.innerHTML = cICL;
+      }
 
-  const cLon = (test.querySelector("longitude")?.textContent) as string;
-    const cLonUpdater = document.getElementById("cTLon");
-  if (cLonUpdater) {
-  cLonUpdater.innerHTML = cLat;
-  } else {
-  console.error("Element with ID 'cLon' not found.");
-  }
+      const cLatUpdater = document.getElementById("cTLat");
+      if (cLatUpdater) {
+        cLatUpdater.innerHTML = cLat;
+      }
+
+      const cLonUpdater = document.getElementById("cTLon");
+      if (cLonUpdater) {
+        cLonUpdater.innerHTML = cLon;
+      }
+
+    });
 
   }
 }
--- D280PA/src/app/mapdata.service.ts
+++ D280PA/src/app/mapdata.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,12 +10,12 @@ export class MapdataService {
 
   constructor(private http: HttpClient) { }
      xmlData = '';
-    getData(countryCode: String){
+    getData(countryCode: String): Observable<Document>{
       const mapUrl = ("https://api.worldbank.org/V2/country/"+ countryCode);
-      this.http.get(mapUrl,{responseType:"text"}).subscribe(data => {this.xmlData=data});
-      const parser = new DOMParser();
-      const xmlDom = parser.parseFromString(this.xmlData, "text/xml");
-      return xmlDom;
+      return this.http.get(mapUrl, { responseType: 'text' })
+      .pipe(
+        map(data => new DOMParser().parseFromString(data, 'text/xml'))
+      );
 
     }
 
