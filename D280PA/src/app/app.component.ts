1dc982649b46a0ebbfe3e3c33a1b9d76a72ffd0b
Jesus <reynosoprof@gmail.com>
2024-11-01 17:28:50 -0700

    added the map to map html. created ttwo colums of display

--- D280PA/src/app/app.component.ts
+++ D280PA/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { TableComponent } from './table/table.component';
+import { MapComponent } from './map/map.component';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet , HomeComponent, TableComponent],
+  imports: [RouterOutlet , HomeComponent, TableComponent,MapComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- D280PA/src/app/home/home.component.ts
+++ D280PA/src/app/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { TableComponent } from '../table/table.component';
+import { MapComponent } from '../map/map.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [TableComponent,MapComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
