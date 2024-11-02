f7f627d3a9f844b07e053377ce4ebc2944607067
Jesus <reynosoprof@gmail.com>
2024-11-02 10:15:37 -0700

    elimated unneseccary table component

--- D280PA/src/app/app.component.ts
+++ D280PA/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { TableComponent } from './table/table.component';
 import { MapComponent } from './map/map.component';
 import { NgModule } from '@angular/core';
 import { map } from 'rxjs';
@@ -9,7 +8,7 @@ import { map } from 'rxjs';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet , HomeComponent, TableComponent,MapComponent],
+  imports: [RouterOutlet , HomeComponent,MapComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- D280PA/src/app/home/home.component.ts
+++ D280PA/src/app/home/home.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { TableComponent } from '../table/table.component';
 import { MapComponent } from '../map/map.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [TableComponent,MapComponent],
+  imports: [MapComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
